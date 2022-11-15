be94f706737815fc9e30b6f158439e805e01bfc2
roetzerfau <maximilian.roetzer@fau.de>
2022-11-15 12:23:36 +0100

    vergessen

--- calculateMucilageDecay.m
+++ calculateMucilageDecay.m
@@ -1,16 +1,17 @@
-function [mucilageConcVector, mucilageVector, rootParticleList, bulkVector] = ...
-    calculateMucilageDecay(g, parameters, bulkVector, mucilageVector, rootParticleList, mucilageConcVector)
+function [mucilageConcVector] = ...
+    calculateMucilageDecay(g, mucilageDecayRate, bulkVector, mucilageVector, mucilageConcVector)
     
     tau = 1;
-    mucilageParticleList = rootParticleList(mucilageVector(rootParticleList) == 1);
+    mucilageParticleList = find(mucilageVector == 1);
     for j = 1 : length(mucilageParticleList)
         ind = mucilageParticleList(j);
 
         st = stencil( g.NX, g.NX, ind, 1);
 
         concOld = mucilageConcVector(ind);
-        decayRate = parameters.mucilageDecayRate * (4-sum(bulkVector(st(2:end))))/4;
-        particleVector = bulkVector-mucilageVector;
+        neigh = (bulkVector +mucilageVector);
+        decayRate = mucilageDecayRate * (4-sum( neigh(st(2:end))))/4;
+        particleVector = bulkVector;
         if( sum(particleVector(st(2:end))) >= 1)
             decayRate = decayRate * 0.3;
         end
@@ -18,21 +19,17 @@ function [mucilageConcVector, mucilageVector, rootParticleList, bulkVector] = ..
         concNew = concOld *  exp(- decayRate * tau);
 
         mucilageConcVector(ind) = concNew;
-
-        if(mucilageConcVector(ind) < parameters.minConcMucilage)
-            mucilageConcVector(ind) = 0;
-        end
     end
   
     
-    indMucilagechanged = find( ( mucilageVector == 1 ) &  ( mucilageConcVector == 0 ) );
-    if(~isempty(indMucilagechanged))
-        mucilageVector(indMucilagechanged) = 0;
-        bulkVector(indMucilagechanged) = 0;
-        for i = 1:numel(indMucilagechanged)
-            rootParticleList = rootParticleList(rootParticleList ~= indMucilagechanged(i));
-        end
-    end
+%     indMucilagechanged = find( ( mucilageVector == 1 ) &  ( mucilageConcVector == 0 ) );
+%     if(~isempty(indMucilagechanged))
+%         mucilageVector(indMucilagechanged) = 0;
+%         bulkVector(indMucilagechanged) = 0;
+%         for i = 1:numel(indMucilagechanged)
+%             rootParticleList = rootParticleList(rootParticleList ~= indMucilagechanged(i));
+%         end
+%     end
 %growing rate verkleiner
 %homgenisieren durch mittelwert aus nachbarn
 end
