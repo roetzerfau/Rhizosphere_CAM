80b0dcbb8cb0b096fb31de574229a24379c96766
Maximilian Rötzer <115470733+roetzerfau@users.noreply.github.com>
2024-06-20 17:41:06 +0200

    Displacement erweiterung

--- EvaluateDisplacement.m
+++ EvaluateDisplacement.m
@@ -1,13 +1,13 @@
 clear; close all;
-a = load('config.5.mat');
-b = load('config.100.mat');
+a = load('FinalConfig/config.5.mat');
+b = load('FinalConfig/config.100.mat');
 N = a.g.NX;
 
-b_root = load('rootConfig.100.mat');
+b_root = load('FinalConfig/rootConfig.100.mat');
 b_root_geo = reshape(b_root.rootVector, [N,N]);
-figure
-imshow(b_root_geo)
-a_root = load('rootConfig.5.mat');
+%figure
+%imshow(b_root_geo)
+a_root = load('FinalConfig/rootConfig.5.mat');
 
 c =  b.particleList(1:2);
 
@@ -15,11 +15,11 @@ r_a = sqrt(sum(a_root.rootVector)/pi);
 r_b = sqrt(sum(b_root.rootVector)/pi);
 r = abs(r_b - r_a);
 
-disp = calculateDisplacementVectors(a.g, a_root.rootVector, b.particleList(1:100), a.particleList(1:100));
+disp = calculateDisplacementVectors(a.g, a_root.rootVector, b.particleList(1:end), a.particleList(1:end));
 
+geoBulk_color = a.bulkVector;
 geoBulk = reshape(a.bulkVector, [N,N]);
 %geoBulk = cat(3, geoBulk, geoBulk, geoBulk);
-
 
 
  
@@ -38,13 +38,32 @@ ind = sub2ind(size(geoBulk), X, Y);
 % geoBulk(row,col,1) = 1;
 % geoBulk(row,col,2:3) = 0;
 geoBulk(ind) = 0.5;
-figure
-imshow(geoBulk)
-hold on
-quiver(Y,X,V,U, 0)
-imwrite(getframe(gcf).cdata, 'displacementParticle.png')
+
+%figure
+%imshow(~geoBulk, [[0.75,0.75,0.75];[1,1,1]])
+%hold on
+%quiver(Y,X,V,U, 0, 'LineWidth',2)
+
+%imwrite(getframe(gcf).cdata, 'displacementParticle.png')%,"ScreenSize",[1000 1060]
 %saveas(double(gcf), 'displacementParticle.png')
 M = sqrt(U.^2 + V.^2);
+
+figure
+for i = 1:numel(M)
+particleInd = a.particleList{i};
+geoBulk_color(particleInd) = geoBulk_color(particleInd) .* (M(i) + 1);
+end
+max_value = max(geoBulk_color);
+%geoBulk_color = geoBulk_color ./(max_value * 255);
+geoBulk_color = reshape(geoBulk_color, [N,N]);
+geoBulk_color = geoBulk_color(1:end,50:150);
+imshow(geoBulk_color, [])
+
+%map = cool;
+%map(1,1:3) = 1;
+map = flipud(hot);
+colormap(map)
+colorbar
 
 d_bar = M/r * 100;
 edges = 0:25:200;
